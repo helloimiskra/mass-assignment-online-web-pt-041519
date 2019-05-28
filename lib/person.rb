596dbd99a26bee033ba2746a48e9b5b820b9de55
Iskra B <ibatistapoblete@gmail.com>
2019-05-28 05:33:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,10 @@
 class Person
-#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-#   :weight, :handed, :complexion, :t_shirt_size,
-#   :wrist_size, :glove_size, :pant_length, :pant_width
+   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 # #list of possible attributes
 #
-#   def initialize(attributes)
-#     attributes.each {|key, value| self.send (("#{key}="), value)}
-#   end
+   def initialize(attributes)
+     attributes.each {|key, value| self.send (("#{key}="), value)}
+   end
 #   #initialize method takes unspecified attributes object
 #   #iterates through each attribute above, and the name of key becomes the setter method,
 #   #w/ attribute becoming the name of value passed through method
