d5be98b3dfe4b65eb68a47a25ff0541179934bdd
Iskra B <ibatistapoblete@gmail.com>
2019-05-28 05:32:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-  :weight, :handed, :complexion, :t_shirt_size,
-  :wrist_size, :glove_size, :pant_length, :pant_width
-#list of possible attributes
-
-  def initialize(attributes)
-    attributes.each {|key, value| self.send (("#{key}="), value)}
-  end
-  #initialize method takes unspecified attributes object
-  #iterates through each attribute above, and the name of key becomes the setter method,
-  #w/ attribute becoming the name of value passed through method
-  #.send will call method name with the value (ie. self.send(key=, value))
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+#   :weight, :handed, :complexion, :t_shirt_size,
+#   :wrist_size, :glove_size, :pant_length, :pant_width
+# #list of possible attributes
+# 
+#   def initialize(attributes)
+#     attributes.each {|key, value| self.send (("#{key}="), value)}
+#   end
+#   #initialize method takes unspecified attributes object
+#   #iterates through each attribute above, and the name of key becomes the setter method,
+#   #w/ attribute becoming the name of value passed through method
+#   #.send will call method name with the value (ie. self.send(key=, value))
 end
 #your code here
